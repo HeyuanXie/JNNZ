ba7a8c646b61031c6f06e60eed89db24d69d413f
HeyuanXie <1114784112@qq.com>
2017-02-22 17:00:12 +0800

    println废弃，替换成print

--- ZhiMaDi/Tools/QNNetworkTool.swift
+++ ZhiMaDi/Tools/QNNetworkTool.swift
@@ -148,7 +148,7 @@ private extension QNNetworkTool {
             }
             catch {
                 completionHandler(request: $0!, response: $1, data: $2, dictionary: nil, error: NSError(domain: "JSON解析错误", code: 10086, userInfo: nil));
-                println("Json解析过程出错")
+                print("Json解析过程出错")
             }
         }
     }
@@ -442,7 +442,7 @@ extension QNNetworkTool {
                     completion(categories: categories, error: nil)
                     HYNetworkCache.save_asyncJsonResponseToCacheFile(array, andURL: "MainCategories", completed: nil)
                 }catch{
-                    println("JSON解析失败")
+                    print("JSON解析失败")
                 }
             }else{
                 completion(categories: nil, error: error)
@@ -564,7 +564,7 @@ extension QNNetworkTool {
                     let categories = ZMDSortCategory.mj_objectArrayWithKeyValuesArray(array)
                     completion(categories:categories,error: nil,dictionary:nil)
                 } catch {
-                    println("Json解析过程出错")
+                    print("Json解析过程出错")
                 }
             }else {
                 completion(categories:nil,error: error,dictionary:nil)
@@ -708,7 +708,7 @@ extension QNNetworkTool {
                     let address = ZMDDSAddress.mj_objectArrayWithKeyValuesArray(array)
                     completion(address: address, error: nil, data: nil)
                 } catch {
-                    println("Json解析过程出错")
+                    print("Json解析过程出错")
                 }
             }else {
                 completion(address: nil, error: error, data: nil)
@@ -1208,7 +1208,7 @@ extension QNNetworkTool {
                     let areas = ZMDArea.mj_objectArrayWithKeyValuesArray(array)
                     completion(areas:areas,error: nil,dictionary:nil)
                 } catch {
-                    println("Json解析过程出错")
+                    print("Json解析过程出错")
                 }
             }else {
                 completion(areas:nil,error: error,dictionary:nil)
